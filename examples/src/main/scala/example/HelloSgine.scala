6f8ff1198df5a0e0739ab8f54f09c12302d4aeaa
Matt Hicks <matt@outr.com>
2015-12-16 10:00:21 -0600

    Removed sample code from HelloSgine.

--- examples/src/main/scala/example/HelloSgine.scala
+++ examples/src/main/scala/example/HelloSgine.scala
@@ -2,23 +2,11 @@ package example
 
 import org.sgine._
 import org.sgine.lwjgl.BasicDesktopApp
-import org.sgine.widget.{Image, Label}
+import org.sgine.widget.Image
 
 object HelloSgine extends BasicDesktopApp {
   this += new Image("sgine.png") {
     position.center := ui.width / 2.0
     position.middle := ui.height / 2.0
   }
-
-  val label = new Label("Hello Sgine!", "OpenSans", "Semibold", 28) {
-    position.center := ui.width / 2.0
-    position.middle := ui.height / 2.0
-  }
-  add(label)
-
-  delay(5.0) andThen function {
-    println("Changing font!")
-    label.font.size := 40
-    label.color := Color.Red
-  } start()
 }
