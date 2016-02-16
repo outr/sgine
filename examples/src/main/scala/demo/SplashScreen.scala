9a5817dbb53862018f6fb267f4572ad6962fe49b
Matt Hicks <matt@outr.com>
2016-02-16 09:59:25 -0600

    Added Sgine logo and updated demo splash screen.

--- examples/src/main/scala/demo/SplashScreen.scala
+++ examples/src/main/scala/demo/SplashScreen.scala
@@ -1,44 +1,40 @@
 package demo
 
 import org.sgine._
+import org.sgine.screen.FPSLoggingSupport
 import org.sgine.transition.easing.Easing
-import org.sgine.widget.{Image, Rect}
+import org.sgine.widget.Image
 
-object SplashScreen extends Screen with VirtualSizeSupport {
+object SplashScreen extends Screen with VirtualSizeSupport with FPSLoggingSupport {
   virtualWidth := 1920.0
   virtualHeight := 1080.0
   virtualMode := VirtualMode.Bars
 
-  val background = new Rect {
-    size.width := ui.width
-    size.height := ui.height
-    color.alpha := 0.0
-    colors.topLeft := Color.DarkBlue
-    colors.topRight := Color.DarkBlue
-    colors.bottomLeft := Color.Black
-    colors.bottomRight := Color.Black
-  }
-
-  val logo = new Image("sgine.png") {
+  val logo = new Image("sgine_highres.png") {
     position.center := ui.center
     position.bottom := ui.height
     scale := 0.0
     color.alpha := 0.0
+    size.maintainAspectRatio(width = 1000.vw)
   }
 
-  add(background)
   add(logo)
 
-  val fadeIn = logo.color.alpha transitionTo 1.0 in 0.5
-  val moveDown = logo.position.middle transitionTo ui.middle.get in 1.0 easing Easing.BackOut
-  val scaleUpX = logo.scale.x transitionTo 1.5 in 1.0
-  val scaleUpY = logo.scale.y transitionTo 1.5 in 1.0
+  val initialDelay = delay(0.1)
+
+  val fadeIn = logo.color.alpha transitionTo 1.0 in 2.0
+  val moveDown = logo.position.middle transitionTo ui.middle.get in 2.0 easing Easing.BackOut
+  val scaleUpX = logo.scale.x transitionTo 1.5 in 2.0
+  val scaleUpY = logo.scale.y transitionTo 1.5 in 2.0
   val bringLogoIn = fadeIn and moveDown and scaleUpX and scaleUpY
 
-  val gradientFadeIn = background.color.alpha transitionTo 1.0 in 0.5
+  val betweenDelay = delay(1.0)
 
-//  gradientFadeIn start ui
-  bringLogoIn andThen gradientFadeIn andThen function {
-    logo.position.middle := ui.middle
-  } start ui
+  val rotateOut = logo.rotation transitionTo 720.0 in 3.0 easing Easing.CircularIn
+  val scaleDownX = logo.scale.x transitionTo 0.0 in 3.0
+  val scaleDownY = logo.scale.y transitionTo 0.0 in 3.0
+  val fadeOut = logo.color.alpha transitionTo 0.0 in 3.5
+  val takeLogoOut = rotateOut and scaleDownX and scaleDownY and fadeOut
+
+  initialDelay andThen bringLogoIn andThen betweenDelay andThen takeLogoOut start ui
 }
