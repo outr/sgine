8c3bc8aaf779b079d03ebe1b09d8fe9354c4f56b
Matt Hicks <matt@matthicks.com>
2022-04-24 20:26:41 -0400

    Uncommenting

--- core/src/main/scala/org/sgine/component/tile/TiledComponent.scala
+++ core/src/main/scala/org/sgine/component/tile/TiledComponent.scala
@@ -1,20 +1,20 @@
-//package org.sgine.component.tile
-//
-//import org.sgine.component._
-//
-//class TiledComponent(tiled: TiledTextureComponent, tileX: Int, tileY: Int) extends Container with DimensionedComponent {
-//  protected lazy val texture: Image = {
-//    x @= tileX * tiled.tileWidth
-//    y @= tileY * tiled.tileHeight
-//    width @= tiled.tileWidth
-//    height @= tiled.tileHeight
-//
-//    val tc = new Image
-//    tc.texture @= tiled.texture
-//    tc
-//  }
-//
-//  override val children: Children[Component] = Children(this, List(texture))
-//
-//  override def toString: String = s"TiledComponent(texture = ${tiled.texture}, tileX = $tileX, tileY = $tileY)"
-//}
+package org.sgine.component.tile
+
+import org.sgine.component._
+
+class TiledComponent(tiled: TiledTextureComponent, tileX: Int, tileY: Int) extends Container with DimensionedComponent {
+  protected lazy val texture: Image = {
+    x @= tileX * tiled.tileWidth
+    y @= tileY * tiled.tileHeight
+    width @= tiled.tileWidth
+    height @= tiled.tileHeight
+
+    val tc = new Image
+    tc.drawable @= tiled.texture
+    tc
+  }
+
+  override val children: Children[Component] = Children(this, Vector(texture))
+
+  override def toString: String = s"TiledComponent(texture = ${tiled.texture}, tileX = $tileX, tileY = $tileY)"
+}
--- core/src/main/scala/org/sgine/component/tile/TiledTextureComponent.scala
+++ core/src/main/scala/org/sgine/component/tile/TiledTextureComponent.scala
@@ -1,39 +1,33 @@
-//package org.sgine.component.tile
-//
-//import com.badlogic.gdx.graphics.Color
-//import org.sgine.component.{Children, Component, Container}
-//import org.sgine.drawable.Texture
-//import reactify._
-//
-//abstract class TiledTextureComponent(val texture: Texture, overflow: Boolean) extends Container {
-//  type T <: TiledComponent
-//
-//  protected def create(tileX: Int, tileY: Int): T
-//
-////  val x: Var[Double] = Var(0.0)
-////  val y: Var[Double] = Var(0.0)
-//  val width: Var[Double] = Var(0.0)
-//  val height: Var[Double] = Var(0.0)
-//  val color: Var[Color] = Var(Color.WHITE)
-//  val tileWidth: Double = texture.width * texture.scaleX
-//  val tileHeight: Double = texture.height * texture.scaleY
-//  lazy val horizontal: Int = width / tileWidth match {
-//    case d if overflow => math.ceil(d).toInt
-//    case d => math.floor(d).toInt
-//  }
-//  lazy val vertical: Int = height / tileHeight match {
-//    case d if overflow => math.ceil(d).toInt
-//    case d => math.floor(d).toInt
-//  }
-//
-//  lazy val tiles: Vector[Vector[T]] = {
-//    scribe.info(s"Building tiles for $horizontal x $vertical - tile: $tileWidth x $tileHeight")
-//    (0 until vertical).toVector.map { tileY =>
-//      (0 until horizontal).toVector.map { tileX =>
-//        create(tileX, tileY)
-//      }
-//    }
-//  }
-//
-//  override lazy val children: Children[Component] = Children(this, tiles.flatten.toList)
-//}
+package org.sgine.component.tile
+
+import org.sgine.component.{Children, Component, Container}
+import org.sgine.drawable.Texture
+import reactify._
+
+abstract class TiledTextureComponent(val texture: Texture, overflow: Boolean) extends Container {
+  type T <: TiledComponent
+
+  protected def create(tileX: Int, tileY: Int): T
+
+  val tileWidth: Double = texture.width * texture.scaleX
+  val tileHeight: Double = texture.height * texture.scaleY
+  lazy val horizontal: Int = width / tileWidth match {
+    case d if overflow => math.ceil(d).toInt
+    case d => math.floor(d).toInt
+  }
+  lazy val vertical: Int = height / tileHeight match {
+    case d if overflow => math.ceil(d).toInt
+    case d => math.floor(d).toInt
+  }
+
+  lazy val tiles: Vector[Vector[T]] = {
+    scribe.info(s"Building tiles for $horizontal x $vertical - tile: $tileWidth x $tileHeight")
+    (0 until vertical).toVector.map { tileY =>
+      (0 until horizontal).toVector.map { tileX =>
+        create(tileX, tileY)
+      }
+    }
+  }
+
+  override lazy val children: Children[Component] = Children(this, tiles.flatten)
+}
