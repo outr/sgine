bf87ca0b1c03483434644d0c18300e8d95039794
Matt Hicks <matt@matthicks.com>
2022-04-29 17:03:09 -0400

    Minor fixes

--- core/src/test/scala/examples/HelloSgine.scala
+++ core/src/test/scala/examples/HelloSgine.scala
@@ -5,12 +5,10 @@ import org.sgine.component.{Component, Image, InteractiveComponent}
 import reactify._
 
 object HelloSgine extends Example {
-  override protected lazy val root: Component = new Image("crate.jpg") with InteractiveComponent {
+  override protected lazy val root: Component = new Image("sgine.png") with InteractiveComponent {
     center @= screen.center
     middle @= screen.middle
 
     color := (if (pointer.over) Color.Red else Color.White)
-
-    override def toString: String = "logo"
   }
 }
