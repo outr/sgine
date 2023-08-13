6178df1a39fa78c216abefd03ac98be76ce3b173
Matt Hicks <matt@matthicks.com>
2023-08-12 19:35:09 -0500

    Started writing TargetingExample

---
+++ core/src/test/scala/examples/TargetingExample.scala
@@ -0,0 +1,13 @@
+package examples
+
+import org.sgine.component.Component
+
+object TargetingExample extends Example {
+  // TODO: floating component on mouse pointer
+  // TODO: multiple enemies on screen
+  // TODO: shows distance on each enemy from component
+  // TODO: highlights nearest target
+  // TODO: outline on component showing targetting range
+
+  override protected def component: Component = ???
+}
