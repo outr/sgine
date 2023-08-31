7c51c00c3cbaf611d5257ca7c4baf27530d2c67a
Matt Hicks <matt@matthicks.com>
2023-08-31 17:45:17 -0500

    Fixed bug in LayoutSupport not accounting for `includeInLayout` changing on a child

--- core/src/main/scala/org/sgine/layout/LayoutSupport.scala
+++ core/src/main/scala/org/sgine/layout/LayoutSupport.scala
@@ -5,8 +5,13 @@ import reactify._
 
 trait LayoutSupport extends AbstractContainer {
   val layout: Var[LayoutManager] = Var(LayoutManager.None)
+  val layoutChildren: Val[Vector[Child]] = Val(children.get.filter(_.includeInLayout))
 
   children.added.attach(c => layout().added(this, c))
   children.removed.attach(c => layout().removed(this, c))
   updates.attach(delta => layout().update(this, delta))
+  layoutChildren.on(layout() match {
+    case l: InvalidatingLayoutManager => l.invalidate()
+    case _ => // Ignore others
+  })
 }
