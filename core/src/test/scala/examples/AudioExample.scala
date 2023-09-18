d26308ebada562c59f9f79f53bfda12eb464e35b
Matt Hicks <matt@matthicks.com>
2023-09-18 14:04:23 -0500

    Preliminary testing of music tasks

--- core/src/test/scala/examples/AudioExample.scala
+++ core/src/test/scala/examples/AudioExample.scala
@@ -2,14 +2,36 @@ package examples
 
 import org.sgine.audio.Audio
 import org.sgine.PointerCursor
+import org.sgine.UI.taskSupport
 import org.sgine.component.{Container, Image, Label, PointerSupport}
 import perfolation.double2Implicits
-
 import reactify._
+import org.sgine.task._
+
+import scala.concurrent.duration.DurationInt
 
 object AudioExample extends Example {
-  private lazy val music = Audio.music("music.mp3")
+  private lazy val music = {
+    val m = Audio.music("music.mp3")
+
+    m.position.once(_ => {
+      sequential(
+        m.volume to 0.0 in 10.seconds,
+        m.volume set 1.0,
+      ).start
+    }, condition = _ > 10.0)
+    m
+  }
   private lazy val sfx = Audio.sound("sfx.mp3")
+
+  // TODO: DSL
+  /*
+  sequential(
+    music.playFadeIn in 5.seconds,
+    music.crossFade to music2 in 10.seconds at end,
+    music2.fadeOutStop at 30.seconds
+  )
+   */
 
   private lazy val logo = new Image("sgine.png") with PointerSupport {
     center @= screen.center
